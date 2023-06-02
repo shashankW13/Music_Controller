cf7033979133d9b26d21679cb42642769ff587e2
Shashank Wadekar <shashankwadekar0@gmail.com>
2023-06-02 16:03:38 +0530

    Matrial UI components and Backend linking

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,4 +1,4 @@
-from .views import RoomView
+from .views import RoomView, CreateRoomView
 from django.urls import path
 
-urlpatterns = [path("room", RoomView.as_view())]
+urlpatterns = [path("room", RoomView.as_view()), path("create-room", CreateRoomView.as_view())]
--- music_controller/frontend/src/index.js
+++ music_controller/frontend/src/index.js
@@ -1,1 +1,12 @@
-import App from "./components/App";
+import App from "./components/App"
+import ReactDOMClient from "react-dom/client"
+import React from "react"
+import { StrictMode } from "react"
+
+const container = document.getElementById("app")
+const root = ReactDOMClient.createRoot(container)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
